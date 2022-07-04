2c33f385c3b673740c475ba83f4499804cfbccc0
min2oyo <min2u@outlook.com>
2022-07-04 21:37:20 +0900

    승패 결과 출력

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import Box from "./components/Box";
 
 function App() {
-	// 객체
+	// DB
 	const choice = {
 		scissor: {
 			name: `Scissor`,
@@ -20,19 +20,45 @@ function App() {
 	};
 
 	// 변수
-	const [MyChoice, setMyChoice] = useState(null);
+	const [UserChoice, setUserChoice] = useState(null);
 	const [ComputerChoice, setComputerChoice] = useState(null);
+	const [UserResult, setUserResult] = useState(null);
+	const [ComputerResult, setComputerResult] = useState(null);
+	const result = [`WIN`, `LOSE`, "TIE"];
 
 	// 함수
 	const play = (choose) => {
 		// 나의 선택
-		setMyChoice(choose);
+		setUserChoice(choose);
 
 		// 컴퓨터 선택
 		const itemArray = Object.keys(choice);
 		const random = Math.trunc(Math.random() * itemArray.length);
 		const computer = itemArray[random];
 		setComputerChoice(choice[computer]);
+
+		// 중간확인
+		console.log(UserChoice.name);
+		console.log(ComputerChoice.name);
+
+		// 승패
+		if (UserChoice.name === ComputerChoice.name) {
+			setUserResult(result[2]);
+			setComputerResult(result[2]);
+		} else if (
+			(UserChoice.name === choice.scissor.name &&
+				ComputerChoice.name === choice.paper.name) ||
+			(UserChoice.name === choice.rock.name &&
+				ComputerChoice.name === choice.scissor.name) ||
+			(UserChoice.name === choice.paper.name &&
+				ComputerChoice.name === choice.rock.name)
+		) {
+			setUserResult(result[0]);
+			setComputerResult(result[1]);
+		} else {
+			setUserResult(result[1]);
+			setComputerResult(result[0]);
+		}
 	};
 
 	// return
@@ -40,8 +66,12 @@ function App() {
 		<>
 			<div className="container">
 				<div className="main-image center">
-					<Box title="You" choose={MyChoice} />
-					<Box title="Computer" choose={ComputerChoice} />
+					<Box title="User" choose={UserChoice} result={UserResult} />
+					<Box
+						title="Computer"
+						choose={ComputerChoice}
+						result={ComputerResult}
+					/>
 				</div>
 				<div className="main-button center">
 					{/* onClick={play()하면 함수가 바로 실행됨} */}
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -6,16 +6,18 @@ const Box = (props) => {
 			<div className="box">
 				<h1>{props.title}</h1>
 				{props.choose !== null ? (
-					<img
-						className="item-img"
-						src={props.choose.img}
-						alt={`${props.title} 이미지`}
-					/>
+					<>
+						<img
+							className="item-img"
+							src={props.choose.img}
+							alt={`${props.title} 이미지`}
+						/>
+						<h2>{props.choose.name}</h2>
+						<div className="result">{props.result}</div>
+					</>
 				) : (
 					<h1>Ready</h1>
 				)}
-				<h2>{props.choose.name}</h2>
-				<div className="result">WIN</div>
 			</div>
 		</>
 	);
